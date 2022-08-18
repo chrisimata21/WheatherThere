cd39c3bb790c337835cc1c2ece6b8f152145bc78
Elizabeth Madrigal <elisabethmadrigal@Elizabeths-MacBook-Pro.local>
2022-08-17 22:22:58 -0700

    added weather api function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,48 @@
+//Live date and time server 
 function showTime() {
     var currentDate = document.getElementById("current-date");
     currentDate.textContent = moment().format('lll');
 } setInterval(showTime, 1000);
 
+// Search Items
+let searchCity = $("#search-city");
+let searchHistory = $("#search-history");
+let searchBtn = $("#btn"); 
+
 //save it in LS
-localStorage.setItem(search, );
+localStorage.setItem(search, history);
 
 //Loads any data saved in local storage 
+$(searchCity).val(localStorage.getItem(getLocationInfo));
 
-
-// Implement google maps functionality
-const axios = require("axios");
-
-const options = {
-  method: 'GET',
-  url: 'https://google-maps28.p.rapidapi.com/maps/api/place/queryautocomplete/json',
-  params: {input: 'pa', language: 'en'},
-  headers: {
-    'X-RapidAPI-Key': '706abaea43msh336890f2b436a99p1e6e5cjsn5bbb1059416b',
-    'X-RapidAPI-Host': 'google-maps28.p.rapidapi.com'
-  }
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-
+// Constant URL value API
+const RAPIDAPI_API_URL = 'https://google-maps28.p.rapidapi.com/maps/api/place/queryautocomplete/json';
+    
+// Function to get location informaton
+let getLocationInfo = function(cityName) {
+    // make api call to get longitude and latitude position of the city
+    let apiUrl = "https://weatherapi-com.p.rapidapi.com/future.json" + cityName + "&limit=1&appid=" + apiKey;
+    fetch(apiUrl).then(function (response) {
+      if(response.ok) {
+        response.json().then(function (data) {
+          // match found
+          if(data.length === 1) {
+            addSearchHistoryItem(data[0].name);
+  
+            currentDayInfo.eq(0).find("h2").text(data[0].name + " (" + moment().format("l") + ")");
+            let lat = data[0]["lat"];
+            let lon = data[0]["lon"];
+  
+            // Using latitude and longitude coordinates to get weather info
+            getWeatherInfo(lat,lon);
+          }
+          else{
+            window.alert("Not a valid city or results not found");
+          }
+        })
+      }
+      else {
+        window.alert("Not a valid city or results not found");
+      }
+    });
+}
