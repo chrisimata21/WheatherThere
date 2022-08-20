1ad80896892081ba91addfdba8537000d07c441f
chrisimata21 <chrisimata21@gmail.com>
2022-08-20 13:01:25 -0700

    added more javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,50 @@ function showTime() {
   var currentDate = document.getElementById("current-date");
   currentDate.textContent = moment().format('lll');
 } setInterval(showTime, 1000);
+let apiKey = ("ff22cce05ddb83c3402fd5ac3516da43");
+// city search
+let searchCity = $("#destination");
+let searchBtn = $("#btn");
+// weather info
+let currentWeather = $("#current-weather")
 
-//save it in LS
-localStorage.setItem(search, );
-
-//Loads any data saved in local storage 
-
-
-// Implement google maps functionality
-const axios = require("axios");
-
-const options = {
-method: 'GET',
-url: 'https://google-maps28.p.rapidapi.com/maps/api/place/queryautocomplete/json',
-params: {input: 'pa', language: 'en'},
-headers: {
-  'X-RapidAPI-Key': '706abaea43msh336890f2b436a99p1e6e5cjsn5bbb1059416b',
-  'X-RapidAPI-Host': 'google-maps28.p.rapidapi.com'
+// function to get city from search bar
+let getCity = function() {
+  if(!searchCity.val()) {
+    window.alert("No city was entered");
+    return;
+  }
+  let cityName = searchCity.val();
+  searchCity.val('');
+  getLocationInfo(cityName);
 }
-};
 
-axios.request(options).then(function (response) {
-console.log(response.data);
-}).catch(function (error) {
-console.error(error);
-});
+// function to get location informaton
+let getLocationInfo = function(cityName) {
+  // make api call to get longitude and latitude position of the city
+  let apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + apiKey;
+  fetch(apiUrl).then(function (response) {
+    if(response.ok) {
+      response.json().then(function (data) {
+        // match found
+        if(data.length === 1) {
+          addSearchHistoryItem(data[0].name);
+
+          currentDayInfo.eq(0).find("h2").text(data[0].name + " (" + moment().format("l") + ")");
+          let lat = data[0]["lat"];
+          let lon = data[0]["lon"];
+
+          // Using latitude and longitude coordinates to get weather info
+          getWeatherInfo(lat,lon);
+        }
+        else{
+          window.alert("Not a valid city or results not found");
+        }
+      })
+    }
+    else {
+      window.alert("Not a valid city or results not found");
+    }
+  });
+  console.log(cityName);
+}
